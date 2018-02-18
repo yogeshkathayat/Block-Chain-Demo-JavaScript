2030e6f965ded79aa067e36b499a210045f8a266
yogesh kathayat <yogesh.kathayat@edfora.com>
2018-02-19 00:29:39 +0530

    added package.json and the main.js file

---
+++ main.js
@@ -0,0 +1,71 @@
+
+var crypto = require('crypto-js');
+
+// a block containing index,timestamp,data,previousHash,hash as fields
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+    //function to calculate hash using crypto
+    calculateHash() {
+        return crypto.SHA256(this.index + this.previousHash, this.timestamp + JSON.stringify(this.data)).toString();
+
+    }
+
+}
+
+
+//blockchain chain containing blocks
+class BlockChain {
+    constructor() {
+        this.chain=[this.createGenesisBlock()];
+    }
+
+    //function to create genesis block
+    createGenesisBlock(){
+        return new Block(0,"20/02/2018","this is genesis block","0");
+    }
+    //function to get latest block
+    getLatestBlock(){
+        return this.chain[this.chain.length-1];
+
+    }
+    //function to add new block into the blockchain
+    addBlock(newBlock){
+
+        newBlock.previousHash=this.getLatestBlock().hash;
+        newBlock.hash=newBlock.calculateHash();
+        this.chain.push(newBlock);
+
+    }
+}
+
+// initializing new blockchain
+let myblockchain=new BlockChain();
+
+
+//adding some dummy blocks
+myblockchain.addBlock(new Block(1,"21/02/2018","another new block"));
+myblockchain.addBlock(new Block(2,"22/02/2018","another new block"));
+myblockchain.addBlock(new Block(3,"23/02/2018","another new block"));
+
+console.log(JSON.stringify(myblockchain,null,4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
